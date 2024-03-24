8983b8ae76aae0f6f94db1206d6ef00fc8fad4cf
GAYEONG PARK <rkdud5483@gmail.com>
2024-03-24 23:48:53 +0100

    24March(16th)

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
-import DetailedProduct from "../components/products/DetailedProduct";
+import DetailedProduct from "../components/Products/DetailedProduct";
 
-export default function product() {
+export default function Product() {
   return (
     <div>
       <Navbar />
